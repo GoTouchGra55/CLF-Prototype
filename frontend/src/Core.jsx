75f681363e2c5a1e6cf70a7c4a29300d851b4151
GoTouchGra55 <shauryatamang2009@gmail.com>
2025-09-15 13:21:23 +0545

    Setup classifier

--- frontend/src/Core.jsx
+++ frontend/src/Core.jsx
@@ -17,7 +17,7 @@ const Core = () => {
   };
 
   return (
-    <div className="flex flex-col sm:flex-row items-center">
+    <div className="flex flex-col sm:flex-row items-center gap-6 md:gap-30 mt-2">
       <div>
         <Webcam
           audio={false}
@@ -25,17 +25,23 @@ const Core = () => {
           mirrored={true}
           videoConstraints={Constraints}
           screenshotFormat="image/jpeg"
-          className="border-1 rounded-2xl shadow-3xl shadow-black"
+          className="border-1 rounded-2xl"
         />
         <Button onClick={capture} className="w-full mt-3 py-6 text-2xl">
-          Capture
+          Predict
         </Button>
       </div>
       <div>
         {captImage && (
-          <div>
-            <h1>Captured Image:</h1>
-            <img src={captImage} alt="captured" />
+          <div className="mb-5 sm:mb-0">
+            <img
+              src={captImage}
+              alt="captured"
+              className="border-1 rounded-2xl"
+            />
+            <h1 className="text-2xl w-full mt-3 py-2 bg-green-600 rounded-[7px] text-center">
+              Predicted: Human(89%)
+            </h1>
           </div>
         )}
       </div>
